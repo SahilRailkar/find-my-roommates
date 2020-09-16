53db0b5e73b1d7d0b51a9109186f2edc5f7291c9
SahilRailkar <30790706+SahilRailkar@users.noreply.github.com>
2020-09-15 22:27:51 -0700

    Fixed padding in Profile page

--- server/index.js
+++ server/index.js
@@ -5,6 +5,11 @@ const cors = require('cors')
 const app = express()
 const port = 8000
 
+const db = require('./db')
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+const userRouter = require('./routes/user')
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cors())
@@ -12,5 +17,7 @@ app.use(cors())
 app.get('/', (req, res) => {
     res.send('Hello, world!')
 })
+
+app.use(userRouter)
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
